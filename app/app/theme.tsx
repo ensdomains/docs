13e6eb5e6bd50b7c074fcc48dedf3420d2080cac
Luc <luc@lucemans.nl>
2024-02-22 04:26:57 +0000

    Update thorin

--- app/app/theme.tsx
+++ app/app/theme.tsx
@@ -5,21 +5,28 @@ import '@ens-tools/thorin-core';
 import { setupConfig } from '@ens-tools/thorin-core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
-import { goerli, mainnet, sepolia } from 'viem/chains';
+import { goerli, holesky, mainnet, sepolia } from 'viem/chains';
 import { createConfig, http, WagmiProvider } from 'wagmi';
-import { injected } from 'wagmi/connectors';
+import { injected, walletConnect } from 'wagmi/connectors';
 
 const config = createConfig({
-    chains: [mainnet, goerli, sepolia],
-    connectors: [injected({})],
+    chains: [mainnet, goerli, sepolia, holesky],
+    connectors: [
+        injected({}),
+        walletConnect({
+            projectId: 'b451d5ff25d61b3fde7b30f167a5a957',
+            showQrModal: false,
+        }),
+    ],
     transports: {
         [mainnet.id]: http(),
         [goerli.id]: http(),
         [sepolia.id]: http(),
+        [holesky.id]: http(),
     },
 });
 
-setupConfig(config);
+setupConfig(config as any);
 
 declare module 'wagmi' {
     interface Register {
