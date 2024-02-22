edbf881fefed737b8f62521112b5d5f06ed191c9
Luc <luc@lucemans.nl>
2024-02-22 06:40:27 +0000

    Bump

--- app/app/theme.tsx
+++ app/app/theme.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { useEffect } from 'react';
 import { goerli, holesky, mainnet, sepolia } from 'viem/chains';
-import { Config, createConfig, http, WagmiProvider } from 'wagmi';
+import { createConfig, http, WagmiProvider } from 'wagmi';
 import { injected, walletConnect } from 'wagmi/connectors';
 
 const config = createConfig({
