dcf8423535b837d6910b8c7cafda1f78c7d6c465
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-01-13 12:13:26 -0500

    Init vocs

---
+++ vocs.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Docs',
+  rootDir: 'src',
+  // sidebar: [
+  //   {
+  //     text: 'Getting Started',
+  //     link: '/getting-started',
+  //   },
+  //   {
+  //     text: 'Example',
+  //     link: '/example',
+  //   },
+  // ],
+})
