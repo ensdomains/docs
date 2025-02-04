982beb60dd5cc030ab08eaf884c22e4f48b222bb
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-02-04 17:20:04 -0500

    Add /web/avatars page

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -143,7 +143,7 @@ export default defineConfig({
             },
             {
               text: 'Avatars',
-              link: '',
+              link: '/web/avatars',
             },
             {
               text: 'Primary Names',
