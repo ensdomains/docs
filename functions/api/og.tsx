1c27580cbb096aad9f3b5f2fc5cc4e5d002a265d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-03-13 14:55:19 -0400

    Test vercel/og

--- functions/api/og.tsx
+++ functions/api/og.tsx
@@ -1,6 +1,24 @@
+import { ImageResponse } from '@vercel/og'
+
 export function onRequest({ request }: { request: Request }) {
   const params = new URL(request.url).searchParams
   const title = params.get('title')
 
-  return Response.json({ title })
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: 128,
+          background: 'lavender',
+        }}
+      >
+        {title ?? 'No Title'}
+      </div>
+    )
+  )
 }
