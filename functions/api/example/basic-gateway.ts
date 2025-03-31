ca49d3119f67be53f3cf6d538216cb3eb228429e
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-03-31 18:32:52 +0800

    Fix basic CCIP Read example gateway

--- functions/api/example/basic-gateway.ts
+++ functions/api/example/basic-gateway.ts
@@ -9,10 +9,15 @@ const router = CcipReadRouter({
 })
 
 router.add({
-  type: 'function addr(bytes32 node) pure returns (address)',
+  type: 'function addr(bytes32 node) external view returns (address)',
   handle: async ([_node]) => {
+    // Always return the same address (registry.ens.eth)
     return ['0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e' as const]
   },
 })
 
-export default { ...router }
+export async function onRequestPost({ request }: { request: Request }) {
+  const body = await request.json()
+  const res = await router.call(body)
+  return Response.json(res)
+}
