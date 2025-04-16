923e947758a54d74cf09e6954a92b4fc02414a50
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-04-16 17:38:19 -0400

    Support decoding DNS names

--- src/components/NameProcessing.tsx
+++ src/components/NameProcessing.tsx
@@ -1,7 +1,8 @@
 import { useMemo, useState } from 'react'
-import { keccak256, namehash, toHex } from 'viem'
+import { isHex, keccak256, namehash, toHex } from 'viem'
 import { normalize, packetToBytes } from 'viem/ens'
 
+import { bytesToPacket } from '../lib/utils'
 import { Input } from './ui/Input'
 
 export function NameProcessing() {
@@ -20,57 +21,80 @@ export function NameProcessing() {
   return (
     <div className="flex flex-col gap-6">
       <Input
-        label="Name"
+        label="Name (string or DNS encoded)"
         value={name}
         onChange={(e) => setName(e.target.value)}
         error={normalizedName === ''}
       />
 
-      {name !== normalizedName && !!normalizedName && (
-        <Input
-          label="Normalized name"
-          value={`${normalizedName}`}
-          disabled
-          copyButton
-        />
-      )}
+      {(() => {
+        // Decode DNS name if it's a hex string
+        if (isHex(name)) {
+          return (
+            <Input
+              label="Decoded name"
+              value={bytesToPacket(name)}
+              disabled
+              copyButton
+            />
+          )
+        }
 
-      <Input label="Label" value={label} disabled copyButton />
+        // Show all name encodings
+        return (
+          <>
+            {name !== normalizedName && !!normalizedName && (
+              <Input
+                label="Normalized name"
+                value={`${normalizedName}`}
+                disabled
+                copyButton
+              />
+            )}
 
-      <Input
-        label="Labelhash"
-        value={label ? keccak256(toHex(label)) : namehash('')}
-        disabled
-        copyButton
-      />
+            <Input label="Label" value={label} disabled copyButton />
 
-      <Input
-        label="Labelhash number"
-        value={BigInt(keccak256(toHex(label))).toString()}
-        disabled
-        copyButton
-      />
+            <Input
+              label="Labelhash"
+              value={label ? keccak256(toHex(label)) : namehash('')}
+              disabled
+              copyButton
+            />
 
-      <Input
-        label="DNS Encode"
-        value={toHex(packetToBytes(normalizedName))}
-        disabled
-        copyButton
-      />
+            <Input
+              label="Labelhash number"
+              value={
+                label
+                  ? BigInt(keccak256(toHex(label))).toString()
+                  : BigInt(namehash('')).toString()
+              }
+              disabled
+              copyButton
+            />
 
-      <Input
-        label="Namehash"
-        value={namehash(normalizedName)}
-        disabled
-        copyButton
-      />
+            <Input
+              label="DNS Encode"
+              value={toHex(packetToBytes(normalizedName))}
+              disabled
+              copyButton
+            />
 
-      <Input
-        label="Namehash number"
-        value={BigInt(namehash(normalizedName)).toString()}
-        disabled
-        copyButton
-      />
+            <Input
+              label="Namehash"
+              value={namehash(normalizedName)}
+              disabled
+              copyButton
+            />
+
+            <Input
+              label="Namehash number"
+              value={BigInt(namehash(normalizedName)).toString()}
+              disabled
+              copyButton
+            />
+          </>
+        )
+      })()}
     </div>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,31 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { type Hex } from 'viem'
+import { bytesToString, hexToBytes } from 'viem/utils'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function truncateAddress(address: `0x${string}`) {
+export function truncateAddress(address: Hex) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`
+}
+
+export function bytesToPacket(bytesString: Hex): string {
+  const bytes = hexToBytes(bytesString)
+  let offset = 0
+  let result = ''
+
+  while (offset < bytes.length) {
+    const len = bytes[offset]
+    if (len === 0) {
+      offset += 1
+      break
+    }
+
+    result += `${bytesToString(bytes.subarray(offset + 1, offset + len + 1))}.`
+    offset += len + 1
+  }
+
+  return result.replace(/\.$/, '')
 }
