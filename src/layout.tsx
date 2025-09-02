e5768841e4206597578e3438c29736539287ace8
Greg <35093316+gskril@users.noreply.github.com>
2025-09-02 17:33:00 -0400

    Remove Intercom (#464)

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren } from 'react'
-import { IntercomProvider } from 'react-use-intercom'
 import { WagmiProvider, createConfig, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 
@@ -16,11 +15,7 @@ const queryClient = new QueryClient()
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <WagmiProvider config={config}>
-      <IntercomProvider autoBoot appId="re9q5yti">
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </IntercomProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   )
 }
