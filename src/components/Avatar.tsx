e7fc767b9b24763f514d1202bffc2568f8ae97e3
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-07-09 21:31:46 -0400

    Revert "Improve avatars"
    This reverts commit a74c3eac68de53c6015ddf84c0fd5fa8df900043.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,5 @@
+import { useEnsAvatar } from 'wagmi'
+
 import { cn } from '../lib/utils'
 
 type Properties = {
@@ -6,30 +8,18 @@ type Properties = {
   rounded?: boolean
 }
 
-// Keeping this in case we ever want to revert back to an RPC instead of Greg's API
-// export function Avatar({ name, width, rounded }: Properties) {
-//   const { data: ensAvatar } = useEnsAvatar({ name, chainId: 1 })
-
-//   return (
-//     <object
-//       data={ensAvatar ?? '/img/fallback-avatar.svg'}
-//       type="image/jpeg"
-//       width={width}
-//       height={width}
-//       className={cn('aspect-square object-cover', rounded && 'rounded-full')}
-//     >
-//       <img src="/fallback-avatar.svg" />
-//     </object>
-//   )
-// }
-
 export function Avatar({ name, width, rounded }: Properties) {
+  const { data: ensAvatar } = useEnsAvatar({ name, chainId: 1 })
+
   return (
-    <img
-      src={`https://ens-api.gregskril.com/avatar/${name}?width=${width * 2}`}
+    <object
+      data={ensAvatar ?? '/img/fallback-avatar.svg'}
+      type="image/jpeg"
       width={width}
       height={width}
       className={cn('aspect-square object-cover', rounded && 'rounded-full')}
-    />
+    >
+      <img src="/fallback-avatar.svg" />
+    </object>
   )
 }
